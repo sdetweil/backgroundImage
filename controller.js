93c22e49d00d1ee43e0f7338a6e8f2f32cfe1b28
Sam Detweiler <sdetweil@gmail.com>
2020-01-18 06:51:16 -0600

    add support for using blurred current image as background of current image

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 var mime = require("mime-types");
 var electron=require('electron')
 var ngrepeat = true;
+var debug=true;
 
 //directive for image loaded event
 
@@ -9,7 +10,7 @@ angular.module("SmartMirror").directive('imageLoaded', function () {
 		restrict: 'A',
 		link: function(scope, element, attrs) {
 			element.bind("load" , function(e){
-				console.log("image loaded")
+				if(debug) {console.log("image loaded")}
 				loadHandler(e);
 			});
 		}
@@ -18,63 +19,66 @@ angular.module("SmartMirror").directive('imageLoaded', function () {
 var biscope=null;
 var timeout = null;
 var sservice= null;
-var imagecycle = 15*1000;			
+var imagecycle = 15*1000;
 var timer_handle = null;
 angular.module("SmartMirror") //,['ngAnimate']
-	.controller("BackgroundImageViewerController", 
-    function ($scope, $http, $timeout, BackgroundImageViewerService) {
+	.controller("BackgroundImageViewerController",
+		function ($scope, $http, $timeout, BackgroundImageViewerService) {
 
-	var atomScreen  = null
-	  if( electron.screen == undefined )
-		{atomScreen = electron.remote.screen}  
-	else
+			var atomScreen	= null
+			if( electron.screen == undefined )
+			{atomScreen = electron.remote.screen}
+			else
 			{atomScreen = electron.screen}
 
-	var mainScreen =atomScreen.getPrimaryDisplay();
-	var dimensions = mainScreen.size;    
-	var counter = 0;
+			var mainScreen =atomScreen.getPrimaryDisplay();
+			var dimensions = mainScreen.size;
+			var counter = 0;
 
-	console.log("window size w="+dimensions.width+" h="+dimensions.height);
-	$scope.screen= {}
-	sw=$scope.screen.width=dimensions.width
-	sh=$scope.screen.height=dimensions.height
-	biscope=$scope;
-	biscope.biimages=[]
-	biscope.biindex=-1;
-      // load all the sources images
+			if(debug) {console.log("window size w="+dimensions.width+" h="+dimensions.height);}
+			$scope.screen= {}
+			sw=$scope.screen.width=dimensions.width
+			sh=$scope.screen.height=dimensions.height
+			biscope=$scope;
+			biscope.bk_fill = config.BackgroundImageViewer.fill;
+			biscope.biimages=[]
+			biscope.biindex=-1;
+			// load all the sources images
 
-	timeout= $timeout;
-			
-	$scope.$watch('focus', () => {
-        // if the new focus is sleep
-		if($scope.focus ==='sleep'){ 
-          // hide the current image
-			biscope.biimages[biscope.biindex].show=false;
-		} else {
-          // only after 1st time notice.. initial setting fires watch handler
-			if(counter >0){ 
-					  // only if no timer running
-					  if(timer_handle == null) {
-						LoadNextImage(BackgroundImageViewerService, biscope);
+			timeout= $timeout;
+
+			$scope.$watch('focus', () => {
+				// if the new focus is sleep
+				if($scope.focus ==='sleep'){
+					// hide the current image
+					biscope.biimages[biscope.biindex].show=false;
+				} 
+				else 
+				{
+				// only after 1st time notice.. initial setting fires watch handler
+					if(counter >0){
+					// only if no timer running
+						if(timer_handle == null) {
+							LoadNextImage(BackgroundImageViewerService, biscope);
+						}
 					}
-			}
-		}
-	})
-	sservice = BackgroundImageViewerService;
-	imagecycle=config.BackgroundImageViewer.cycle*1000
+				}
+			})
+			sservice = BackgroundImageViewerService;
+			imagecycle=config.BackgroundImageViewer.cycle*1000
 
-	let p = BackgroundImageViewerService.loadImages($scope)
-	Promise.all(p).then(
-         () =>{ 
-				        console.log(" images loaded, start display")
-	biscope.biimages=BackgroundImageViewerService.getImageList();
-	biscope.biindex=0;
-	LoadNextImage( BackgroundImageViewerService, biscope);
-	counter =1;
-}
-         );
-} 
-  );
+			let p = BackgroundImageViewerService.loadImages($scope)
+			Promise.all(p).then(() =>{
+				if(debug) {console.log(" images loaded, start display")}
+				biscope.biimages=BackgroundImageViewerService.getImageList();
+				biscope.biindex=0;
+
+				LoadNextImage( BackgroundImageViewerService, biscope);
+				counter =1;
+			}
+			);
+		}
+	);
 
 
 
@@ -86,15 +90,15 @@ function ScaleImage(srcwidth, srcheight, targetwidth, targetheight, fLetterBox) 
 		return result;
 	}
 
-    // scale to the target width
+	// scale to the target width
 	var scaleX1 = targetwidth;
 	var scaleY1 = (srcheight * targetwidth) / srcwidth;
 
-    // scale to the target height
+	// scale to the target height
 	var scaleX2 = (srcwidth * targetheight) / srcheight;
 	var scaleY2 = targetheight;
 
-    // now figure out which one we should use
+	// now figure out which one we should use
 	var fScaleOnWidth = (scaleX2 > targetwidth);
 	if (fScaleOnWidth) {
 		fScaleOnWidth = fLetterBox;
@@ -124,89 +128,109 @@ function loadHandler(evt){
 	let index= parseInt(evt.currentTarget.id.substring(3));
 	let img1 = evt.currentTarget
 	biscope.biimages[index].img=img1
-	let m = parseInt(window.getComputedStyle(document.body,null).getPropertyValue('margin-top'));        
- //console.log("image loaded="+img1.src+" size="+img1.width+":"+img1.height);
+	let m = parseInt(window.getComputedStyle(document.body,null).getPropertyValue('margin-top'));
+	//if(debug) console.log("image loaded="+img1.src+" size="+img1.width+":"+img1.height);
 
 	if(img1.style.backgroundSize == ""){
-    // with the size of this image and it's parent
-    // compute the new size and offsets
+		// with the size of this image and it's parent
+		// compute the new size and offsets
 		let result = ScaleImage(img1.naturalWidth, img1.naturalHeight, sw, sh, true);
-    // adjust the image size
+		// adjust the image size
 		img1.width = result.width;
 		img1.height = result.height;
 
-    // adjust the image position
+		// adjust the image position
 		img1.style.left = result.targetleft+"px";
 		img1.style.top = result.targettop+"px";
-    //console.log("load image info ="+JSON.stringify(result))
+		//if(debug) console.log("load image info ="+JSON.stringify(result))
 	}
 	else{
 		img1.width = sw;
 		img1.height = sh-1;
 	}
 
-	img1.style.position="absolute";      
-	img1.style.opacity = 1;  
+	img1.style.position="absolute";
+	img1.style.opacity = 1;
 	img1.style.margin="auto";
 	img1.parentElement.style.left="0px";
 	img1.parentElement.style.top="0px";
 	img1.parentElement.style.width=sw+"px";
 	img1.parentElement.style.height=sh+"px";
-	img1.parentElement.style.backgroundColor = config.BackgroundImageViewer.background;
-
-	img1.style.backgroundColor="#808080";
-	img1.style.transition = "opacity 1.25s";
-	biscope.biimages[index].show=true;  	
-	if(index>0) {
+	if(config.BackgroundImageViewer.fill == false){
+		img1.parentElement.style.backgroundColor = config.BackgroundImageViewer.background;
+	}
+	else{
+		img1.parentElement.style.background ="transparent";
 		
+		let fill_bk=img1.parentElement.parentElement.getElementsByClassName('bgimage')[0]
+		
+		fill_bk.style['filter']="blur("+config.BackgroundImageViewer.blur+"px)";
+		fill_bk.style["-webkit-filter"]="blur("+config.BackgroundImageViewer.blur+"px)";
+		fill_bk.style.backgroundImage = "url("+img1.src+")";
+		fill_bk.style.height=sh+"px"
+		fill_bk.style.width=sw+"px";
+		img1.parentElement.style.backgroundColor="rgba(0,0,0,0)"
+	}
+
+	//img1.style.backgroundColor="#808080";
+	img1.style.transition = "opacity 1.25s";
+	biscope.biimages[index].show=true;
+	if(index>0) {
+
 		biscope.biimages[index-1].show=false;
 		biscope.biimages[index-1].active=false;
 		biscope.$apply();
-		biscope.biimages[index-1].img.style="display:none";
+		if(biscope.biimages[index-1].img)
+		{biscope.biimages[index-1].img.style="display:none";}
 	}
 
-	console.log("imagecycle delay time="+imagecycle)
+	if(debug) {console.log("imagecycle delay time="+imagecycle)}
 	if(timer_handle == null) {
 		timer_handle=timeout(() => {
-			//console.log("timer")
+			//if(debug) console.log("timer")
 			timer_handle = null;
 			LoadNextImage(sservice,biscope)
 		}, imagecycle)
 	}
 	else{
-		 console.log("timer handle not null");
+		 if(debug) {console.log("timer handle not null");}
 	}
 }
 function LoadNextImage(service, scope){
-	console.log(" starting next image load")
-  // only change images if visual focus is set. ( not sleep, etc)
+	if(debug) {console.log(" starting next image load")}
+	// only change images if visual focus is set. ( not sleep, etc)
 	if(scope.focus !== 'sleep') {
-    // if we reached the end of the list
+		// if we reached the end of the list
 		if(scope.biindex>=scope.biimages.length){
+			scope.biindex=0;
 			let p = service.loadImages(scope)
 			Promise.all(p).then( () => {
 				scope.biimages=service.getImageList();
-        // reset to the start again
+				// reset to the start again
 				if(scope.biindex==scope.biimages.length ){
 					scope.biimages[scope.biindex-1].show=false;
-					scope.biimages[scope.biindex-1].active=false;  
+					scope.biimages[scope.biindex-1].active=false;
 					scope.$apply();
 				}
 				scope.biindex=0;
-				scope.biimages[scope.biindex].active=true;        
-				scope.biindex++;       
-        //console.log("restart")          
+				scope.biimages[scope.biindex].active=true;
+				scope.biindex++;
+				//if(debug) console.log("restart")
 			}
-      )      
+			)
 		}
 		else {
-			if(scope.biindex>0){        
+			// if we are on the second or higher
+			if(scope.biindex>0){
+				// hide the prior image
 				scope.biimages[scope.biindex-1].show=false;
-				scope.biimages[scope.biindex-1].active=false;  
+				scope.biimages[scope.biindex-1].active=false;
 			}
-			scope.biimages[scope.biindex].active=true;          
-			scope.biindex++;  
-			console.log("new image ")
-		}  
-	} 
+			// show next one, angular active=true will inject the html
+			// causing the image load
+			scope.biimages[scope.biindex].active=true;
+			scope.biindex++;
+			if(debug) {console.log("new image ")}
+		}
+	}
 }
--- service.js
+++ service.js
@@ -1,111 +1,111 @@
 
 angular.module("SmartMirror")
 	.factory("BackgroundImageViewerService", 
-  function ($http, $interval) {
-	var enabledTypes = ["image/jpeg", "image/png", "image/gif"];
-	var biservice= {}
-	var imageIndex=-1;
-	var images = [];
+		function ($http, $interval) {
+			var enabledTypes = ["image/jpeg", "image/png", "image/gif"];
+			var biservice= {}
+			var imageIndex=-1;
+			var images = [];
   
-	const getShuffledArr = arr => {
-		const newArr = arr.slice()
-		for (let i = newArr.length - 1; i > 0; i--) {
-			const rand = Math.floor(Math.random() * (i + 1));
-			[newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
-		}
-		return newArr
-	}; 
-   
-	function getImages(path) {
-    //console.log("looking for images in"+path);
-		let p = new Promise((resolve,reject) =>{
-			try {
-				fs.readdir(path,
-          function (error, files) {
-	let filelist=[]
-	if(error == null ){              
-		for(file of files){
-			let f = path + "/" + file
-			if (! fs.statSync(f).isDirectory() ) {
-				let type=mime.lookup(file)
-				if (type !== false && enabledTypes.indexOf(type) >= 0 ) {
-					filelist.push({file:f, show:false, active:false});
+			const getShuffledArr = arr => {
+				const newArr = arr.slice()
+				for (let i = newArr.length - 1; i > 0; i--) {
+					const rand = Math.floor(Math.random() * (i + 1));
+					[newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
 				}
+				return newArr
+			}; 
+   
+			function getImages(path) {
+				console.log("looking for images in"+path);
+				let p = new Promise((resolve,reject) =>{
+					try {
+						fs.readdir(path,
+							function (error, files) {
+								let filelist=[]
+								if(error == null ){              
+									for(file of files){
+										let f = path + "/" + file
+										if (! fs.statSync(f).isDirectory() ) {
+											let type=mime.lookup(file)
+											if (type !== false && enabledTypes.indexOf(type) >= 0 ) {
+												filelist.push({file:f, show:false, active:false});
+											}
+										}
+									}
+								}
+								resolve(filelist)
+							}
+						);
+					}
+					catch(error)
+					{
+						if(error=='EHOSTDOWN')
+						{
+							reject();
+						} 
+					}
+				})
+				return p;
 			}
-		}
-	}
-	resolve(filelist)
-}
-        );
-			}
-			catch(error)
-      {
-				if(error=='EHOSTDOWN')
-         {
-					reject();
-				} 
-			}
-		})
-		return p;
-	}
 
-  // load all images from all sources into one list
-	biservice.loadImages= function(scope){
-		let promises= []
-		images = [];
-    // loop thru the sources
-		for(var source of config.BackgroundImageViewer.sources)
-    {
-			let p = new Promise((resolve,reject) =>{
-				let path = __dirname + '/plugins'+'/backgroundImage/'+source.path;
-        // get the images for this source
-        //console.log("getting images from path="+path+" source="+source)
-				getImages(path).then((imagelist) =>{ 
-          // save the source images to the main list
-					images=images.concat(imagelist)         
-          // restart the list at the beginning
-					source.index=0;
-					imageIndex=0;
-          // indicate we are done for this source
-					resolve();
-				})
-			})
-			promises.push(p);      
-		}
-		return promises;
-	}
-  // get next image from the list
-	biservice.next= function(scope){
-		var promise = new Promise((resolve, reject) => {
-      // if we have more images
-			if(imageIndex>=0 && imageIndex<images.length) {    
-        // return next
-				resolve(images[imageIndex++])
+			// load all images from all sources into one list
+			biservice.loadImages= function(scope){
+				let promises= []
+				images = [];
+				// loop thru the sources
+				for(var source of config.BackgroundImageViewer.sources)
+				{
+					let p = new Promise((resolve,reject) =>{
+						let path = __dirname + '/plugins'+'/backgroundImage/'+source.path;
+						// get the images for this source
+						//console.log("getting images from path="+path+" source="+source)
+						getImages(path).then((imagelist) =>{ 
+							// save the source images to the main list
+							images=images.concat(imagelist)         
+							// restart the list at the beginning
+							source.index=0;
+							imageIndex=0;
+							// indicate we are done for this source
+							resolve();
+						})
+					})
+					promises.push(p);      
+				}
+				return promises;
 			}
-			else {
-        // no more images
-				images=[];
-				imageIndex=-1;
-        // refresh the list
-				var p= service.loadImages(scope)
-				Promise.all(p).then(() => {        
-					service.getImageList();
-					imageIndex=0;
-          // return next image
-					resolve(images[imageIndex++])
+			// get next image from the list
+			biservice.next= function(scope){
+				var promise = new Promise((resolve, reject) => {
+					// if we have more images
+					if(imageIndex>=0 && imageIndex<images.length) {    
+						// return next
+						resolve(images[imageIndex++])
+					}
+					else {
+						// no more images
+						images=[];
+						imageIndex=-1;
+						// refresh the list
+						var p= service.loadImages(scope)
+						Promise.all(p).then(() => {        
+							service.getImageList();
+							imageIndex=0;
+							// return next image
+							resolve(images[imageIndex++])
+						})
+					}
 				})
+				//return promise;    
 			}
-		})
-		return promise;    
-	}
-	biservice.getImageList = function(){
-		// if shuffle is requested
-		if(config.BackgroundImageViewer.shuffle){
-			// shuffle the list
-			images=getShuffledArr(images)
+			biservice.getImageList = function(){
+				// if shuffle is requested
+				if(config.BackgroundImageViewer.shuffle){
+					// shuffle the list
+					images=getShuffledArr(images)
+				}
+				return images;
+			}  
+			return biservice  
 		}
-		return images;
-	}  
-	return biservice  
-}
-);
+	);
